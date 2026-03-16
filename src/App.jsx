cad8973eec20ed99ec240bd3839bea5f00ee0533
tanzid-48 <mdtanzid.525@gmail.com>
2026-03-16 23:25:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Countries from './Countries/Countries'
+
+function App() {
+
+
+  return (
+    <>
+  
+     <Countries></Countries>
+        
+         <div className="">
+          <h3>TanZid</h3>
+         </div>
+         
+      
+       
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h2>Thank you</h2>
+        </div>
+    );
+};
+
+export default Countries;
