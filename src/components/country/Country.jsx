e3c4e4eae114e26f49c614b0d42e43c5146080f1
tanzid-48 <mdtanzid.525@gmail.com>
2026-03-17 11:41:18 +0600

    Add rundaring Css Styles

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './country.css'
+
 
 const Country = ({country}) => {
 
+    const [visited,setVisited] =useState(false)
+
      const handleVisited = () =>{
+    //    setVisited(visited ? false : true)
+    setVisited(!visited)
 
      }
     return (
-        <div className='country'>
+        // <div className={`country ${visited ? 'country-visited':'' }`}>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt}/>
             <h2>Name: {country.name.common} </h2>
             <h3>Population: {country.population.population}</h3>
             <h4>Continents:{country.continents.continents}</h4>
             <h3>Area: {country.area.area}  
              { country.area.area > 300000 ? " Big Country" : " Small Country"}</h3>
-             <button onClick={handleVisited}>Not Visited</button>
+             <button onClick={handleVisited}>
+                {
+                    visited ? "Visited" : "Not Visited"
+                }
+             </button>
 
         </div>
     );
