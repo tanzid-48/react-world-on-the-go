132f815a757af8b56bd2520bed19b654abca24b9
tanzid-48 <mdtanzid.525@gmail.com>
2026-03-17 11:15:05 +0600

    Add style like design and some rundaring condition and add a button to handle visited country

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,18 +1,21 @@
 import React, { use } from 'react';
 import Country from '../country/Country';
+import './countries.css'
 
-const Countries = ({countryPromise}) => {
+const Countries = ({ countryPromise }) => {
 
     const countriesData = use(countryPromise)
     const countries = countriesData.countries
-   
+
     return (
         <div>
-          <h1>In The Country : {countries.length}</h1>
-         
-         {
-            countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
-         }
+            <h1>In The Country : {countries.length}</h1>
+
+            <div className="countries">
+                {
+                    countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+                }
+            </div>
 
         </div>
     );
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import './country.css'
 
 const Country = ({country}) => {
+
+     const handleVisited = () =>{
+
+     }
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt}/>
             <h2>Name: {country.name.common} </h2>
             <h3>Population: {country.population.population}</h3>
             <h4>Continents:{country.continents.continents}</h4>
-            <h3>Area: {country.area.area}</h3>
+            <h3>Area: {country.area.area}  
+             { country.area.area > 300000 ? " Big Country" : " Small Country"}</h3>
+             <button onClick={handleVisited}>Not Visited</button>
 
         </div>
     );
